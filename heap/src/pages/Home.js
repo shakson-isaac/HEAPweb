70496acf8776a54bc2e3c84d62bb386831844822
shakson-isaac <shakson_isaac@g.harvard.edu>
2025-03-17 14:08:53 -0400

    HEAP graphical abstract home page

--- heap/src/pages/Home.js
+++ heap/src/pages/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
       <h1>Welcome to HEAP!</h1>
       <p>This webpage provides HEAP summary statistics generated from:</p>
       <p>UK Biobank Genetics, Exposures, and Proteomics</p>
-      <img src="/HEAP_summ.tif" alt="HEAP Logo" style={{ width: '5000px', marginTop: '20px' }} />
+      <img src="/HEAP_summ.png" alt="HEAP Logo" style={{ width: '5000px', marginTop: '20px' }} />
     </div>
   );
 };
