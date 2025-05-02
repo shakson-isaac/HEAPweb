465f730907cb1672bad52387e0cce46441644b82
shakson-isaac <shakson_isaac@g.harvard.edu>
2025-05-02 14:23:25 -0400

    Correct binding port for flask app

--- heap/backend/app.py
+++ heap/backend/app.py
@@ -437,4 +437,5 @@ def page_not_found(e):
     return jsonify(error="Page not found"), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port, debug=True)
